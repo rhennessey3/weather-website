874ae79653b71dfba806f77c336d53f059ab9b62
Richard Hennessey III <Rick@Richards-iMac.local>
2020-07-03 18:03:49 -0400

    deployed app

--- src/app.js
+++ src/app.js
@@ -4,9 +4,6 @@ const hbs = require('hbs')
 const forecast = require('../utils/forecast')
 const geocode = require('../utils/geocode')
 
-
-
-
 // console.log(__dirname)
 // console.log(path.join(__dirname, '../public'))
 // nodemon src/app.js -e js,hbs  (use this in terminal to flag partials for restart with nodemon)
