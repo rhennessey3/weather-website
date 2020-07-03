07651038f6b5ff27a438a8523d0ec176e470e688
Richard Hennessey III <Rick@Richards-iMac.local>
2020-07-03 14:21:17 -0400

    init commit

---
+++ web-server/src/app.js
@@ -0,0 +1,96 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const forecast = require('../utils/forecast')
+const geocode = require('../utils/geocode')
+
+
+// console.log(__dirname)
+// console.log(path.join(__dirname, '../public'))
+// nodemon src/app.js -e js,hbs  (use this in terminal to flag partials for restart with nodemon)
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Rick Hennessey'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Rick Hennessey'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Need Some help?',
+        name: 'Rick Hennessey',
+        helpMessage: 'Get in contact with me to get some help.'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'you must provide an address'
+        })
+    }
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: 'oops not found',
+        name: 'Rick Hennessey',
+        error: 'page not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 'looks like you found a leak',
+        name: 'Rick Hennessey',
+        error: 'page not found'
+    })
+})
+
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
+
+
